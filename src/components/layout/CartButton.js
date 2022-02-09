edf9e0a65dc401c0bf41dd5a41ea7cf872e9a527
Huthaifa <h.j.salman@students.ptuk.edu.ps>
2022-02-09 11:42:17 +0200

    added animation for the header cart button using useEffect

--- src/components/layout/CartButton.js
+++ src/components/layout/CartButton.js
@@ -1,16 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from './CartButton.module.scss';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from "../../store/cart-context";
 
 const CartButton = (props) => {
+    const [btnIsHighlited, setBtnIsHighlited] = useState(false);
     const cartCtx = useContext(CartContext);
     const { items } = cartCtx;
     const cartAmount = items.reduce((num, curr) => {
         return num + curr.amount
     }, 0);
+    const btnClasses = `${styles.button} ${btnIsHighlited ? styles.bump : ''}`;
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlited(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlited(false);
+        }, 300)
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
